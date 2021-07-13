5a9a77f34abd25a89ea61881078c3cb336e3282e
Edip Çiftçi <ediler1970@gmail.com>
2021-07-13 16:32:36 +0300

    computeButton function is updated to respond to non-positive numbers

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 /* 
-  Function to compute the ending year of the interest and amount of interest.
-  Activates when clicked on "computeButton".
+  Function activates when clicked on "computeButton".
   Uses the following input from the user: Principal money amount, rate of interest,
 number of years for payback.
-  Gives an output string of these values to print as the text of "result".
+  It has three objectives.
+  First is to compute the ending year of the interest and amount of interest.
+  Second is to give an output string of these values to print as the text of "result".
+  Third is to check whether the principal value is positive or not. If it is not,
+alerts the user and focuses on the principal box.
 */
 
 document.getElementById("computeButton").onclick = function compute() {
@@ -12,6 +15,11 @@ document.getElementById("computeButton").onclick = function compute() {
   var years = document.getElementById("years").value;
   var interest = (principal * years * rate) / 100;
   var year = new Date().getFullYear() + parseInt(years);
+  if (principal <= 0){
+    alert("Enter a positive number");
+    setTimeout(function(){document.getElementById("principal").focus();}, 1);
+  }
+  else{
   var text =
     "If you deposit " +
     principal.toString().bold() +
@@ -22,7 +30,8 @@ document.getElementById("computeButton").onclick = function compute() {
     ",\nin the year " +
     year.toString().bold();
   var result = document.getElementById("result");
-  result.innerText = text;
+  result.innerText = text;var principal = document.getElementById("principal");
+  }
 };
 
 /*
@@ -33,18 +42,4 @@ document.getElementById("computeButton").onclick = function compute() {
 document.getElementById("rate").onchange = function updateRate() {
   var rateval = document.getElementById("rate").value;
   document.getElementById("rate_val").innerText = rateval + "%";
-};
-
-/*
-  Function to check whether the principal value is positive or not.
-  If it is not, alerts the user.
-  Activates when the value of "principal" is changed.
-*/
-
-document.getElementById("principal").onchange = function checkPositive() {
-  var principal = document.getElementById("principal");
-  if (principal.value <= 0){
-    alert("Enter a positive number");
-    setTimeout(function(){principal.focus();}, 1);
-  }
 };
